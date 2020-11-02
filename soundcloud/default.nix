e86ec1eedcd2b68b956003e165f7f09b19a8774b
Jonathan Strickland <djanatyn@gmail.com>
2020-11-02 03:03:50 -0500

    no nix for soundcloud

--- soundcloud/default.nix
+++
@@ -1,7 +0,0 @@
-let
-  pkgs = import (import ./nix/sources.nix).nixpkgs {
-    config = { allowUnfree = true; };
-  };
-
-  fetchsoundcloud = pkgs.callPackage ./fetchsoundcloud.nix { };
-in { playlists = import ./playlists.nix { inherit fetchsoundcloud; }; }
